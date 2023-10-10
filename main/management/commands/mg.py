1654f28b9b8fdd33b0cf0a16c1af65a90b2e846c
nurayferecli <nuraynecefli2001@gmail.com>
2023-10-10 23:34:50 +0400

    first commit

---
+++ main/management/commands/mg.py
@@ -0,0 +1,13 @@
+from typing import Any
+from django.core.management import BaseCommand,call_command
+
+
+class Command(BaseCommand):
+    help = ''
+    
+    
+    
+    def handle(self, *args: Any, **options: Any):
+        
+        call_command('makemigrations')
+        call_command('migrate')
